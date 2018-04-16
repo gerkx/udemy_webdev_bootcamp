334cea7955a9bd1411868758f0192228f8899147
Patrick Gerke <ptgerke@gmail.com>
2018-04-17 00:24:21 +0200

    intro to javascript

---
+++ js_script_demo/demo.js
@@ -0,0 +1,12 @@
+// var first = prompt("what's your first name?");
+// var last = prompt("what's your last name?");
+var age = prompt("How old are you?");
+
+// var nameSentence = "Your name is " + first + " " + last + ".";
+// var ageSentence = "You are " + age + " years old.";
+
+var days = age*365.25;
+
+console.log(age + "years old is aprox " + days + " days old.");
+
+
