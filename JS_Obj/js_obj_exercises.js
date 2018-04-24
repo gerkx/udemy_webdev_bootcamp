aa62eb7c493dbfe5f58936c366fdbafd89ed6fb2
Patrick Gerke <ptgerke@gmail.com>
2018-04-24 15:40:29 +0200

    js object exercises

---
+++ JS_Obj/js_obj_exercises.js
@@ -0,0 +1,14 @@
+var person = {
+    name: "Pat",
+    age: 36,
+    city: "Portland",
+}
+console.log(person);
+
+person.name = "Patrick";
+console.log("//////////////////////");
+console.log(person);
+person.age += 1;
+console.log("//////////////////////");
+console.log(person);
+
