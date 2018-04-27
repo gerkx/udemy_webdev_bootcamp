4927e4dced88a86ff7ae7102fe9a1f2a35c1a0ad
Patrick Gerke <ptgerke@gmail.com>
2018-04-27 17:37:56 +0200

    jQuery basic exercises

---
+++ jQuery/jQuery-methods.js
@@ -0,0 +1,9 @@
+$("h1").text("boop");
+
+$("img").css("width", "300px");
+
+$("img:first-of-type").attr("src", "https://c3.staticflickr.com/3/2418/2243463214_f32ab004af_b.jpg");
+
+$("img").last().attr("src", "https://c3.staticflickr.com/3/2418/2243463214_f32ab004af_b.jpg");
+
+$("input").val("Patrick Gerke");
---
+++ jQuery/jquery-demo.js
@@ -0,0 +1,11 @@
+var styles = {
+    color: "red",
+    border: "2px solid green"
+}
+$("li").css(styles);
+
+$("li").css("color", "purple");
+
+$("#two").css(
+    {fontSize: "30px"}
+);
