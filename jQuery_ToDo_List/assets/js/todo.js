00581c0bde30fd56f6662028d155718958bab7db
Patrick Gerke <ptgerke@gmail.com>
2018-05-04 01:00:07 +0200

    added ability to add and remove items

--- jQuery_ToDo_List/assets/js/todo.js
+++ jQuery_ToDo_List/assets/js/todo.js
@@ -1,4 +1,21 @@
 // check off specific todos by clicking
-$('li').click(function(){
+$('ul').on("click", "li", function(){
     $(this).toggleClass("completed");
+});
+
+// click on x to delete item
+
+$('ul').on("click", ".trash", function(event){
+    $(this).parent().fadeOut(333,function(){
+        $(this).remove;
+    });
+    event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){
+        var newItemText = $(this).val();
+        $(this).val("");
+        $("ul").append("<li> <span class='trash'>X</span> " + newItemText + "</li>");
+    }
 });
